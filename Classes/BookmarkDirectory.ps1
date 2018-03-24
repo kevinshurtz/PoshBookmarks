05375675b201f6c554f3fa658e32b0c598479f91
kshurtz <kevin.shurtz@kevinshurtz.net>
2018-03-24 14:00:44 -0500

    Enable and Disable functions removed, Enable automatic

--- Classes/BookmarkDirectory.ps1
+++ Classes/BookmarkDirectory.ps1
@@ -2,7 +2,9 @@
 Class BookmarkDirectory {
     [System.Collections.Generic.Dictionary[String, System.IO.DirectoryInfo]] $Bookmarks;
     [System.IO.DirectoryInfo] $LastSessionDirectory;
-    [Int32] $EventSubscriberId;
+
+    [Int32] $eventSubscriberId;
+    [DateTime] $saveLocationLastUpdate;
     
     static [String] $defaultSaveLocation = "$env:HOMEPATH\Documents\WindowsPowerShell\SessionData\SessionBookmarks.clixml";
     static [BookmarkDirectory] $singleBookmarkDir;
@@ -20,7 +22,16 @@ Class BookmarkDirectory {
 
     static [BookmarkDirectory] GetInstance() {
         if ([BookmarkDirectory]::singleBookmarkDir -eq $null) {
-            [BookmarkDirectory]::singleBookmarkDir = [BookmarkDirectory]::new([BookmarkDirectory]::defaultSaveLocation)
+            [BookmarkDirectory]::singleBookmarkDir = [BookmarkDirectory]::new([BookmarkDirectory]::defaultSaveLocation);
+
+            # Save bookmarks on exit
+            $saveBookmarksEventSubscriber = Register-EngineEvent PowerShell.Exiting -Action {
+                [BookmarkDirectory]::GetInstance().UpdateLastSessionDirectory((Get-Location).Path);
+                [BookmarkDirectory]::GetInstance().SaveBookmarkDirectory([BookmarkDirectory]::defaultSaveLocation);
+            }
+
+            # Save event subscriber ID for potential later deregistration
+            $[BookmarkDirectory]::singleBookmarkDir.eventSubscriberId = $saveBookmarksEventSubscriber.Id;
         }
 
         return [BookmarkDirectory]::singleBookmarkDir;
@@ -28,7 +39,16 @@ Class BookmarkDirectory {
 
     static [BookmarkDirectory] GetCustomInstance([String] $customLocation) {
         if ([BookmarkDirectory]::singleBookmarkDir -eq $null) {
-            [BookmarkDirectory]::singleBookmarkDir = [BookmarkDirectory]::new($customLocation)
+            [BookmarkDirectory]::singleBookmarkDir = [BookmarkDirectory]::new($customLocation);
+
+            # Save bookmarks on exit
+            $saveBookmarksEventSubscriber = Register-EngineEvent PowerShell.Exiting -Action {
+                [BookmarkDirectory]::GetInstance().UpdateLastSessionDirectory((Get-Location).Path);
+                [BookmarkDirectory]::GetInstance().SaveBookmarkDirectory($customLocation);
+            }
+
+            # Save event subscriber ID for potential later deregistration
+            $[BookmarkDirectory]::singleBookmarkDir.eventSubscriberId = $saveBookmarksEventSubscriber.Id;
         }
 
         return [BookmarkDirectory]::singleBookmarkDir;
@@ -39,43 +59,32 @@ Class BookmarkDirectory {
         $this.LastSessionDirectory = $path;
     }
 
-    [void] UpdateLastSessionDirectory() {
-        $this.LastSessionDirectory = (Get-Location).Path;
-    }
-
     # Write bookmark data to disk, typically invoked before exit
     [void] SaveBookmarkDirectory([String] $path) {
         try {
-            Export-Clixml -Path $path -InputObject $this;
+            if ($this.promptOverwriteConfiguration()) {
+                Export-Clixml -Path $path -InputObject $this;
+            }
         }
         catch [System.IO.DirectoryNotFoundException] {
             Write-Information "Specfied directory does not exist.  Creating path...";
-            New-Item -ItemType 'File' -Force -Name $path
-            Write-Information "New location created."
-        }
-    }
-
-    [void] SaveBookmarkDirectory() {
-        try {
-            Export-Clixml -Path ([BookmarkDirectory]::defaultSaveLocation) -InputObject $this;
-        }
-        catch [System.IO.DirectoryNotFoundException] {
-            Write-Information "Specfied directory does not exist.  Creating path...";
-            New-Item -ItemType 'File' -Force -Name ;
-            Write-Information "New location created."
+            New-Item -ItemType 'File' -Force -Name $path;
+            Write-Information "New location created.";
         }
     }
 
     # Load previous bookmark data, typically at the beginning of a session
     [void] LoadBookmarkDirectory([String] $path) {
         try {
+            # Save the last write time of the file
+            $this.saveLocationLastUpdate = (Get-Item -Path $path).LastWriteTime;
+
             # PSObject with bookmark directory properties from prior session
             $deserializedBookmarks = Import-Clixml $path;
             
             # The Bookmarks property in the CLIXML file cannot be deserialized directly into a Dictionary
             foreach ($bookmarkKey in $deserializedBookmarks.Bookmarks.Keys) {
-                [System.IO.DirectoryInfo] $bookmarkValue =
-                [System.IO.DirectoryInfo]::new(
+                [System.IO.DirectoryInfo] $bookmarkValue = [System.IO.DirectoryInfo]::new(
                     $deserializedBookmarks.Bookmarks[$bookmarkKey].FullName);
                 $this.Bookmarks.Add($bookmarkKey, $bookmarkValue);
             }
@@ -91,5 +100,41 @@ Class BookmarkDirectory {
         catch [System.IO.FileNotFoundException] {
             Write-Warning "Specifed file does not exist.  No bookmark data loaded.";
         }
+    }
+
+    [void] DisableSaveOnExit() {
+        # Unregister the event to save bookmark data
+        Get-EventSubscriber `
+        |   Where-Object Id -eq [BookmarkDirectory]::singleBookmarkDir.eventSubscriberId `
+        |   Unregister-Event;
+    }
+
+    # Prompt the user to overwrite the bookmark configuratino, default to $true
+    [bool] promptOverwriteConfiguration() {
+        $title = "Override Bookmark Configuration";
+        $message = "
+            Another PowerShell session has updated your Bookmark configuration.
+            Would you like to overwrite this update with the Bookmark configuration
+            from this session?";
+        
+        $yes = [System.Management.Automation.Host.ChoiceDescription]::new(
+            "&Yes", "Overwrites the PowerShell Bookmark configuration updated from another session");
+
+        $no = [System.Management.Automation.Host.ChoiceDescription]::new(
+            "&No", "Keeps the existing PowerShell Bookmark configuration updated from another session");
+
+        $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no);
+
+        $result = $(Get-Host).UI.PromptForChoice($title, $message, $options, 0);
+
+        # Return $true if the user approves of overwriting the updated configuration
+        switch ($result)
+        {
+            0 { return $true }
+            1 { return $false }
+        }
+
+        # Return $true by default
+        return $true;
     }
 }
--- Functions/Disable-Bookmarks.ps1
+++
@@ -1,5 +0,0 @@
-function Disable-Bookmarks {
-    # Unregister the event to save bookmark data
-    $bookmarks = [BookmarkDirectory]::GetInstance()
-    Get-EventSubscriber | Where-Object Id -eq $bookmarks.EventSubscriberId | Unregister-Event
-}
